a12fe16418ba553d1d1403ea594d0f1bfcb32776
khilendra2003-dewangan <khilendra24dewangan@gmail.com>
2026-03-29 12:39:28 +0530

    add frontend end url

--- backend/index.js
+++ backend/index.js
@@ -18,8 +18,9 @@ app.use(cors({
         "http://localhost:5173", 
         "http://localhost:5174", 
         "http://localhost:5175", 
-        "http://localhost:3000"
-    ],
+        "http://localhost:3000",
+        process.env.FRONTEND_URL
+    ].filter(Boolean),
     credentials: true,
 }));
 app.use(express.json());
